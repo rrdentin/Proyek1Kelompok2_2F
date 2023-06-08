8ad942c0b77d719d133a2cdc7cdf916ad37b2b61
MuhammadEgaRam <2141720143@student.polinema.ac.id>
2023-06-08 16:54:50 +0700

    Update Sidebar - Shadow flag

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,7 +1,7 @@
 <!-- Main Sidebar Container -->
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar-dark-primary">
     <!-- Brand Logo -->
-    <a href="admin.dashboard" class="brand-link">
+    <a href="dashboard" class="brand-link">
         <img src="{{ asset('/') }}dist/img/Logo Shaleh.png" alt="AdminLTE Logo"
             class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light"> Anak Shaleh </span>
