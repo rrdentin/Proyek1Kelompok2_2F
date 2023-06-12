4bbc80c9a9969ad46efd9fbfc7582ba8ac01915a
rrdentin <denti.dona@gmail.com>
2023-06-12 07:48:23 +0700

    Update User Models - ver 4

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,4 +56,9 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->belongsTo(Level::class, 'level');
     }
+
+    public function pendaftars()
+    {
+    return $this->hasMany(Pendaftar::class);
+    }
 }
