8339c9f8b34a8d405c0ff76b6f5fd9daf5cbdec4
rrdentin <denti.dona@gmail.com>
2023-06-12 08:54:29 +0700

    Update Models Siswa - ver 3

--- app/Models/Siswa.php
+++ app/Models/Siswa.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class Siswa extends Model
 {
@@ -14,16 +16,44 @@ class Siswa extends Model
     protected $fillable = [
         'pendaftar_id', 
         'name',
-        'jenKel', 
-        'alamat', 
-        'tglLahir', 
-        'foto', 
-        'kk', 
-        'akte'
+        'jenKel',
+        'alamat',
+        'tglLahir',
+        'foto',
+        'kk',
+        'akte',
+        'jenjangPend',
+        'nik',
+        'tempatLahir',
     ];
 
     public function pendaftar()
     {
         return $this->belongsTo(Pendaftar::class);
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::saving(function ($model) {
+            $validationRules = [
+                'name' => [
+                    'required',
+                    Rule::unique('siswas')->where(function ($query) use ($model) {
+                        return $query->where(function ($query) use ($model) {
+                            $query->where('tempatLahir', $model->tempatLahir)
+                                ->orWhere('tglLahir', $model->tglLahir);
+                        })->orWhere('name', $model->name);
+                    }),
+                ],
+            ];
+
+            $validator = Validator::make($model->attributesToArray(), $validationRules);
+
+            if ($validator->fails()) {
+                throw new \Exception($validator->errors()->first());
+            }
+        });
     }
 }
